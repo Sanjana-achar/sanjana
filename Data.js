0fbc951e3205fdfa880acba387567663eaaa939d
Sanjana-achar <138116009+Sanjana-achar@users.noreply.github.com>
2023-07-19 01:09:06 +0530

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,59 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Infinix",
+      price: 145,
+      desc: "4 GB RAM | 64 GB ROM | Expandable Upto 2 TB",
+      img: "https://www.phoneworld.com.pk/price/wp-content/uploads/2021/08/M3P-5G-1536x1536.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "iphone 13",
+      price: 712,
+      desc: "128 GB ROM |15.49 cm (6.1 inch) Super Retina XDR Display",
+      img: "https://www.mistershopking.com/12470/apple-iphone-13-pro-max-128gb-graphite-italia.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Oppo f15",
+      price: 225,
+      desc: "4 GB RAM | 128 GB ROM | Expandable Upto 256 GB|16.26 cm (6.4 inch) Full HD+ Display",
+      img: "https://www.mobigyaan.com/wp-content/uploads/2020/01/Oppo-F15.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Samsung s23 ultra",
+      price: 1644,
+      desc: "12 GB RAM | 512 GB ROM |17.27 cm (6.8 inch) Quad HD+ Display 200MP + 10MP + 12MP + 10MP | 12MP Front Camera",
+      img: "https://rukminim2.flixcart.com/image/128/128/xif0q/mobile/m/l/o/-original-imagmg6gzjf7gggt.jpeg?q=70",
+    },
+    {
+      id: "ebooencnlnl",
+      name: "Nokia",
+      price: 85.5,
+      desc: "2 GB RAM | 32 GB ROM | Expandable Upto 128 GB|13.84 cm (5.45 inch) HD+ Display",
+      img: "https://tse1.mm.bing.net/th/id/OIP.ypEKbRekoKKkNq2ya2AQ_QHaHa?w=185&h=185&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
+    {
+      id: "ghxbhenjn",
+      name: "Poco 5G",
+      price: 134,
+      desc: "4 GB RAM | 64 GB ROM | Expandable Upto 512 GB|16.71 cm (6.58 inch) Full HD+ Display",
+      img: "https://i.expansys.net/img/b/363180/xiaomi-poco-m3-pro-5g-dual-sim.jpg",
+    },
+    {
+      id: "qusgdhjc",
+      name: "Realme 11 pro",
+      price: 146,
+      desc: "6 GB RAM | 64 GB ROM | Expandable Upto 1 TB|17.07 cm (6.72 inch)",
+      img: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/0/x/v/-original-imagqadf2awzzmyf.jpeg?q=70",
+    },
+    {
+      id: "picnwgnskjkj",
+      name: "Redmi",
+      price: 292.3,
+      desc: "6 GB RAM | 128 GB ROM|16.94 cm (6.67 inch) Full HD+ AMOLED Display|1.58GHz processor speed",
+      img: "https://tse1.mm.bing.net/th/id/OIP.JdGcGnWBhYfdfYNZ1zDF-AHaHP?w=187&h=183&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    },
+
+  ];
